2c91065aced7f5764ac6c488455e393f9b8fc803
EH.Milon <ehasanulhaquemilon@gmail,com>
2023-01-29 07:50:46 +0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace CSharp
+{
+    class CProgram
+    {
+        static void Main(string[]args)
+        {
+            int[] list=new int[] {8,6,4,5,7,2,1,3};
+            System.Console.WriteLine("The array in ascending order");
+            Array.Sort(list);
+            foreach (int i in list)
+            {
+                Console.Write(i +" ");
+            }
+        }
+    }
+}
+
